e7a6947e987d52a157c7f2df6f6c0467a16dfde1
efrain <efarinva98@gmail.com>
2019-08-26 19:24:56 -0400

    swagger working

---
+++ restaurant/src/main/java/com/lambda/restaurant/model/Restaurant.java
@@ -0,0 +1,11 @@
+package com.lambda.restaurant.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "restaurant")
+//public class Restaurant {
+//
+//
+//}
